418aca97ea0987278f1cabca5e57896e66cbcf94
Raj Kumar <rajkumarbigide461@gmail.com>
2021-07-27 22:55:07 +0530

    First Commit to CountryManager

---
+++ country-manager/src/main/java/com/rajkumar/countrymanager/CountryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rajkumar.countrymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryManagerApplication.class, args);
+	}
+
+}
---
+++ country-manager/src/test/java/com/rajkumar/countrymanager/CountryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajkumar.countrymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
